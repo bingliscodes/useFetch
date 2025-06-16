711f7bb0e4aeeb556cb0fd52549003bf9eb647a0
Benjamin Inglis <bjammin134@gmail.com>
2025-06-16 10:28:31 -0700

    Added button for testing

--- src/components/userListWithFetch.jsx
+++ src/components/userListWithFetch.jsx
@@ -1,6 +1,6 @@
 import useFetch from "../hooks/useFetch.js";
 
-const NUM_SECONDS = 5;
+const NUM_SECONDS = 15;
 
 export default function userListWithFetch() {
   const cacheKey = "userList";
@@ -8,6 +8,7 @@ export default function userListWithFetch() {
     data: users,
     isLoading,
     error,
+    sendRequest,
   } = useFetch(
     "https://jsonplaceholder.typicode.com/users",
     cacheKey,
@@ -18,13 +19,17 @@ export default function userListWithFetch() {
 
   if (error) return <p>Error: {error}</p>;
 
+  // TODO: Could add a button to send request and indicate whether a request was made
   return (
-    <ul>
-      {users.map((user) => (
-        <li key={user.id}>
-          {user.name} ({user.email})
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name} ({user.email})
+          </li>
+        ))}
+      </ul>
+      <button onClick={sendRequest}> Send Request</button>
+    </>
   );
 }
